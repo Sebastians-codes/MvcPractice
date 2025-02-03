b6b62185fc825dc901fa7673a27180ee5e8fd91e
Sebastian <Sebastianw.codes@gmail.com>
2025-02-03 15:44:49 +0100

    checkpoint

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models;
+
+public class Category
+{
+    public int Id { get; set; }
+    [MaxLength(30)] [Required] public string Name { get; set; }
+    [Range(1, 100)] [Required] public int DisplayOrder { get; set; }
+}
